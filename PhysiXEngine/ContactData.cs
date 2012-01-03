172f1ae309d2fd346c113a984d5d81ca2feddb9c
Mohammed Aljobory <mhdaljobory@gmail.com>
2012-01-03 18:42:54 +0300

    Added SphereAndSphere() method which calculate Contact Data

--- PhysiXEngine/ContactData.cs
+++ PhysiXEngine/ContactData.cs
@@ -13,7 +13,7 @@ namespace PhysiXEngine
          * Holds the bodies that are involved in the contact. The
          * second of these can be NULL, for contacts with the scenery.
          */
-        private Body[] body = new Body[2];
+        private Collidable[] body = new Collidable[2];
 
         /**
          * Holds the position of the contact in world coordinates.
@@ -32,9 +32,27 @@ namespace PhysiXEngine
          */
         private double penetration;
 
-        public ContactData(Body firstBody, Body secondBody) {
+        public ContactData(Collidable firstBody, Collidable secondBody)
+        {
             this.body[0] = firstBody;
             this.body[1] = secondBody;
+        }
+
+        public void SphereAndSphere()
+        {
+            //Cache the sphere positions
+            Vector3 positionOne = ((Body)body[0]).position;
+            Vector3 positionTwo = ((Body)body[1]).position;
+
+            // Find the vector between the objects
+            Vector3 midline = positionOne - positionTwo;
+            double size = midline.Length();
+
+            // We manually create the normal, because we have the
+            // size to hand.
+            contactNormal = Vector3.Multiply(midline,(float)(1.0f/size));
+            contactPoint = positionOne + Vector3.Multiply(midline,0.5f);
+            penetration = ((Sphere)body[0]).radius + ((Sphere)body[1]).radius - size;
         }
     }
 }
