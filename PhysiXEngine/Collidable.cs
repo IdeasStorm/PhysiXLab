feb8d7e50be0022fd88778682ce5ef8cf23a6e7d
Muhammad Al-Syrwan <mhdsyrwan@gmail.com>
2012-01-03 18:54:31 +0200

    inherited Collidable from Body

--- PhysiXEngine/Collidable.cs
+++ PhysiXEngine/Collidable.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace PhysiXEngine
 {
-    abstract class Collidable
+    abstract class Collidable : Body
     {
         public ContactData generateContacts(Collidable other);
         public Boolean CollidesWith(Collidable other);
