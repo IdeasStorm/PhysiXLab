dff3764805ebbd253c2aba3750308ee274cce96e
Muhammad Al-Syrwan <mhdsyrwan@gmail.com>
2012-01-03 23:09:51 +0200

    Merge sphere.cs

--- PhysiXEngine/Sphere.cs
+++ PhysiXEngine/Sphere.cs
@@ -5,14 +5,27 @@ using System.Text;
 
 namespace PhysiXEngine
 {
-    class Sphere : Body, Collidable
+    class Sphere : Collidable
     {
         public double radius { public get; protected set; }
 
-        public ContactData generateContacts(Collidable other) {
-            ContactData contactData = new ContactData(this, other);
-            if (other as Sphere != null) {
+        public ContactData generateContacts(Collidable other)
+        {
+            ContactData contactData = null;
+            if (other as Sphere != null)
+            {
+                contactData = new ContactData(this, other);
                 contactData.SphereAndSphere();
+            }
+            if (other as Plane != null)
+            {
+                contactData = new ContactData(this, other);
+                contactData.SphereAndPlane();
+            }
+            if (other as Box != null)
+            {
+                contactData = new ContactData(this, other);
+                contactData.BoxAndSphere();
             }
             return contactData;
         }
