116091800530d5855b9b117e1d518eaa9e42a5d5
Muhammad Al-Syrwan <mhdsyrwan@gmail.com>
2012-01-04 10:21:20 +0200

    Added an Empty Game

---
+++ PhysiXLab/PhysiXLab/Program.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace PhysiXLab
+{
+#if WINDOWS || XBOX
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            using (Lab game = new Lab())
+            {
+                game.Run();
+            }
+        }
+    }
+#endif
+}
+
