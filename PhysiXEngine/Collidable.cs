ce49042e14d4ee6004f151fc6ced4fcfe864a583
Muhammad Al-Syrwan <mhdsyrwan@gmail.com>
2012-01-08 06:31:03 +0200

    overrode update() to update boundings

--- PhysiXEngine/Collidable.cs
+++ PhysiXEngine/Collidable.cs
@@ -8,8 +8,36 @@ namespace PhysiXEngine
 {
     public abstract class Collidable : Body
     {
+        /// <summary>
+        /// generates contact information for this collidable body with another one 
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
         public abstract ContactData generateContacts(Collidable other);
+
+        /// <summary>
+        /// Checks whether the body collides with another collidable or not
+        /// </summary>
+        /// <param name="other">the other collidable body to collides with </param>
+        /// <returns></returns>
         public abstract Boolean CollidesWith(Collidable other);
+
+        /// <summary>
+        /// gets the bounding sphere arround the collidable body 
+        /// </summary>
+        /// <returns>the sphere arround the body's bounding volume</returns>
         public abstract BoundingSphere GetBoundingSphere();
+
+        /// <summary>
+        /// updates the bounding Volume of this Collidable Object
+        /// </summary>
+        protected abstract void updateBounding();
+
+        public override void Update(float duration)
+        {
+            base.Update(duration);
+            updateBounding();
+            //TODO after or before update ?
+        }
     }
 }
