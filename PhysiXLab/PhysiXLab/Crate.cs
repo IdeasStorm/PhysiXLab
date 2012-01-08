76d7a749b714e2d2f8b9b0418f980fdee8e4e577
Tamer-Shlash <mr.tamershlash@gmail.com>
2012-01-08 18:35:04 +0200

    Added crate (game equilivant to Box of Engine)

---
+++ PhysiXLab/PhysiXLab/Crate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PhysiXEngine;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace PhysiXLab
+{
+    public class Crate : Box
+    {
+        public Model model { set; get; }
+
+        public Crate(Vector3 halfSize)
+            : base(halfSize)
+        {
+        }
+
+        public void Draw(Camera camera)
+        {
+            Matrix[] transforms = new Matrix[model.Bones.Count];
+            model.CopyAbsoluteBoneTransformsTo(transforms);
+            foreach (ModelMesh mesh in model.Meshes)
+            {
+                foreach (BasicEffect be in mesh.Effects)
+                {
+                    be.EnableDefaultLighting();
+                    be.World = mesh.ParentBone.Transform * TransformMatrix;
+                    be.View = camera.view;
+                    be.Projection = camera.projection;
+                }
+                mesh.Draw();
+            }
+        }
+    }
+}
