cde71f28c01f4db4c8ee6684c392afac5ca7541a
Muhammad Al-Syrwan <mhdsyrwan@gmail.com>
2012-01-08 04:05:42 +0200

    Added new Paper (collision Enums)

---
+++ Papers/ContactData.cs
@@ -0,0 +1,13 @@
+class ContactData 
+{
+	enum CollisionType
+	{
+		None=0x0,
+		FirstBox=0x10,
+		SecondBox=0x01,
+		FirstSphere=0x20,
+		SecondSphere=0x02,
+		FirstPlane=0x30,
+		SecondPlane=0x03
+	}
+}
