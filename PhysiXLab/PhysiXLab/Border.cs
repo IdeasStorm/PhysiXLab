6e5ff9a9ab933bf9f9dad1bba4ec522cb88348d0
Tamer-Shlash <mr.tamershlash@gmail.com>
2012-01-28 03:49:26 +0200

    Pushing failed test :(

---
+++ PhysiXLab/PhysiXLab/Border.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PhysiXEngine;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Test
+{
+    public class Border : HalfSpace
+    {
+        public Model model { set; get; }
+
+        public Border(Plane plane)
+            :base(plane)
+        {   }
+
+        public void Draw(Camera camera)
+        {
+            Matrix[] transforms = new Matrix[model.Bones.Count];
+            model.CopyAbsoluteBoneTransformsTo(transforms);
+            foreach (ModelMesh mesh in model.Meshes)
+            {
+                foreach (BasicEffect be in mesh.Effects)
+                {
+                    be.EnableDefaultLighting();
+                    be.World = mesh.ParentBone.Transform * Matrix.CreateScale(plane.Normal);
+                    be.View = camera.view;
+                    be.Projection = camera.projection;
+                }
+                mesh.Draw();
+            }
+        }
+    }
+}
--- PhysiXLab/PhysiXLab/Collisions.cs
+++ PhysiXLab/PhysiXLab/Collisions.cs
@@ -21,7 +21,7 @@ namespace Test
 
         #region "testing components"
         Ball b1;
-        HalfSpace p1;
+        Border p1;
         LinkedList<Ball> balls = new LinkedList<Ball>();
         ContactGenerator cg;
         Camera camera;
@@ -44,7 +44,8 @@ namespace Test
             b1.Position = new Vector3(100,0,0);
             b1.model = Content.Load<Model>(@"ball");
 
-            p1 = new HalfSpace(new Plane(new Vector3(100, 0, 0), 100));
+            p1 = new Border(new Plane(new Vector3(50, 50, 50), 10));
+            p1.model = Content.Load<Model>(@"plane");
 
             cg = new ContactGenerator();
             cg.AddBody(b1);
@@ -91,6 +92,7 @@ namespace Test
             b1.Draw(camera);
             foreach (Ball b in balls)
                 b.Draw(camera);
+            p1.Draw(camera);
 
             base.Draw(gameTime);
         }
