438e9285735bad6ee82c4b1d594c26cc423ad4f2
Mohammed Aljobory <mhdaljobory@gmail.com>
2012-01-30 18:11:02 +0300

    Finished Panel Class

--- PhysiXLab/PhysiXLab/Panel.cs
+++ PhysiXLab/PhysiXLab/Panel.cs
@@ -11,7 +11,7 @@ using Microsoft.Xna.Framework.Media;
 using FuchsGUI;
 
 
-namespace Test
+namespace App
 {
     /// <summary>
     /// This is a game component that implements IUpdateable.
@@ -25,22 +25,24 @@ namespace Test
             Buttons = new List<Button>();
             TextBoxes = new List<TextBox>();
             Lables = new List<Label>();
+            Fields = new Dictionary<String, float>();
+            Panels = new Dictionary<String, Form>();
 
             this.Position = position;
             this.Width = width;
             this.Height = height;
             _ButtonPosition = new Point(position.X + 15, position.Y + 15);
             _LabelPosition = new Point(position.X + 15, position.Y + 15);
-            _TexBoxPosition = new Point(position.X + 15, position.Y + 15);
+            _TexBoxPosition = new Point(position.X + 95, position.Y + 15);
         }
 
-        private Point Position;
-        private int Width;
-        private int Height;
+        protected Point Position;
+        protected int Width;
+        protected int Height;
 
-        private Form frm;
+        private Form Frm;
 
-        private Texture2D PenalTexture;
+        private Texture2D PanelTexture;
         private Texture2D ButtonTexture;
         private Texture2D TextBoxTexture;
         private SpriteFont Font;
@@ -56,6 +58,11 @@ namespace Test
         private Point _TexBoxPosition;
         public Point TexBoxPosition { get { return _TexBoxPosition; } set { _TexBoxPosition = value; } }
 
+        public bool Show = false;
+
+        private Dictionary<String, float> Fields;
+        private Dictionary<String, Form> Panels;
+
         SpriteBatch spriteBatch;
         /// <summary>
         /// Allows the game component to perform any initialization it needs to before starting
@@ -64,65 +71,174 @@ namespace Test
         public override void Initialize()
         {
             // TODO: Add your initialization code here
-            
+
             base.Initialize();
         }
-        /*
-        void ButtonClicked(Control sender)
-        {
-            Lables.Last<Label>().Text = sender.Name;
-        }
-         */
 
-        public void AddButton(String buttonName, Point Position, int Width, int Height)
+        void OkButtonClicked(Control sender)
         {
-            Buttons.Add(new Button("bt" + (Buttons.Count + 1), buttonName,
-                new Rectangle(50, 50, 100, 50), ButtonTexture, Font, Color.Black));
-            frm.AddControl(Buttons.Last<Button>());
+            foreach (TextBox tb in TextBoxes)
+            {
+                float val;
+                float.TryParse(tb.Text, out val);
+                Fields[tb.Name] = val;
+            }
+            Show = false;
         }
 
-        public void AddButton(String buttonName = "Button", int width = 70, int height = 20)
+        void CancelButtonClicked(Control sender)
         {
-            Buttons.Add(new Button("bt" + (Buttons.Count + 1), buttonName,
+            Show = false;
+        }
+
+        public void AddButton(String buttonName, String buttonText, String panelName,
+            Point position, int width, int height)
+        {
+
+            Button myButton = new Button(buttonName, buttonText,
+                new Rectangle(position.X, position.Y, width, height), ButtonTexture, Font, Color.Black);
+            Buttons.Add(myButton);
+            if (panelName == "MainPanel")
+                Frm.AddControl(myButton);
+            else
+            {
+                Form myFrm = null;
+                Panels.TryGetValue(panelName, out myFrm);
+                if (myFrm != null)
+                {
+                    myFrm.AddControl(myButton);
+                }
+            }
+            //Frm.AddControl(Buttons.Last<Button>());
+        }
+
+        public void AddButton(String buttonName = "Btn", String buttonText = "Button",
+            String PanelName = "MainPanel", int width = 70, int height = 20)
+        {
+            /*
+            Buttons.Add(new Button(buttonName, buttonText, 
                 new Rectangle(_ButtonPosition.X, _ButtonPosition.Y, width, height), 
-                ButtonTexture, Font, Color.Black));
-            frm.AddControl(Buttons.Last<Button>());
+                ButtonTexture, Font, Color.Black));*/
+            //Frm.AddControl(Buttons.Last<Button>());
+            if (PanelName == "MainPanel")
+            {
+                Button myButton = new Button(buttonName, buttonText,
+                new Rectangle(_ButtonPosition.X, _ButtonPosition.Y, width, height),
+                ButtonTexture, Font, Color.Black);
+                Buttons.Add(myButton);
+                Frm.AddControl(myButton);
+            }
+            else
+            {
+                AddButton(buttonName, buttonText, PanelName, _ButtonPosition, width, height);
+            }
             //Buttons.Last<Button>().onClick += new EHandler(ButtonClicked);
             _ButtonPosition.Y += 25;
         }
 
-        public void AddTextBox(String value, int maxLength, Point position, int width, int height)
+        public void AddTextBox(String textBoxName, String value, String PanelName,
+            int maxLength, Point position, int width, int height)
         {
-            TextBoxes.Add(new TextBox("txt" + (TextBoxes.Count+1), value, maxLength, 
-                new Rectangle(position.X, position.Y, width, height), 
-                TextBoxTexture, Font, Color.Black));
-            frm.AddControl(TextBoxes.Last<TextBox>());
+            TextBox myTextBox = new TextBox(textBoxName, value, maxLength,
+                new Rectangle(position.X, position.Y, width, height),
+                TextBoxTexture, Font, Color.Black);
+            TextBoxes.Add(myTextBox);
+            //Frm.AddControl(TextBoxes.Last<TextBox>());
+            Form myFrm = null;
+            Panels.TryGetValue(PanelName, out myFrm);
+            if (myFrm != null)
+            {
+                myFrm.AddControl(myTextBox);
+            }
         }
 
-        public void AddTextBox(String value = "", int maxLength = 5, int width = 70, int height = 20)
+        public void AddTextBox(String textBoxName = "txt", String value = "", String PanelName = "MainPanel",
+            int maxLength = 5, int width = 70, int height = 20)
         {
-            TextBoxes.Add(new TextBox("txt" + (TextBoxes.Count + 1), value, maxLength,
+
+            //Frm.AddControl(TextBoxes.Last<TextBox>());
+            if (PanelName == "MainPanel")
+            {
+                TextBox myText = new TextBox(textBoxName, value, maxLength,
                 new Rectangle(_TexBoxPosition.X, _TexBoxPosition.Y, width, height),
-                TextBoxTexture, Font, Color.Black));
-            frm.AddControl(TextBoxes.Last<TextBox>());
+                TextBoxTexture, Font, Color.Black);
+                TextBoxes.Add(myText);
+                Frm.AddControl(myText);
+            }
+            else
+            {
+                AddTextBox(textBoxName, value, PanelName, maxLength, _TexBoxPosition, width, height);
+            }
             _TexBoxPosition.Y += 25;
         }
 
-        public void AddLabel(String labelName, Point position)
+        public void AddLabel(String labelName, String labelText, Point position, String PanelName)
         {
-            Lables.Add(new Label("lb" + (Lables.Count + 1), labelName, 
-                new Vector2(position.X, position.Y), Font, Color.White, 15, 2));
-            frm.AddControl(Lables.Last<Label>());
+            Label myLabel = new Label(labelName, labelName, new Vector2(position.X, position.Y),
+                Font, Color.Black, 15, 2);
+            Lables.Add(myLabel);
+            //Frm.AddControl(Lables.Last<Label>());
+            Form myFrm = null;
+            Panels.TryGetValue(PanelName, out myFrm);
+            if (myFrm != null)
+            {
+                myFrm.AddControl(myLabel);
+            }
         }
 
-        public void AddLabel(String labelName = "Label")
+        public void AddLabel(String labelName = "lb", String labelText = "Label", String PanelName = "MainPanel")
         {
-            Lables.Add(new Label("lb" + (Lables.Count + 1), labelName,
+            if (PanelName == "MainPanel")
+            {
+                Label myLabel = new Label(labelName, labelName, new Vector2(_LabelPosition.X, _LabelPosition.Y),
+                Font, Color.Black, 15, 2);
+                Lables.Add(myLabel);
+                Frm.AddControl(myLabel);
+            }
+            else
+            {
+                AddLabel(labelName, labelText, _LabelPosition, PanelName);
+            }
+            /*
+            Lables.Add(new Label(labelName, labelText,
                 new Vector2(_LabelPosition.X, _LabelPosition.Y), Font, Color.White, 15, 2));
-            frm.AddControl(Lables.Last<Label>());
+            Frm.AddControl(Lables.Last<Label>());*/
             _LabelPosition.Y += 25;
         }
 
+        public void AddField(String name, float value, String panelName = "MainPanel")
+        {
+            Fields.Add(name, value);
+            AddLabel(name, name, panelName);
+            //_TexBoxPosition.X += 80;
+            AddTextBox(name, value.ToString(), panelName);
+        }
+
+        public void AddPanel(String panelName, Point position, int width, int height)
+        {
+            Panels.Add(panelName, new Form(panelName, "",
+                new Rectangle(position.X, position.Y, width, height),
+                PanelTexture, Font, Color.White));
+        }
+
+        public void AddOkButton(String panelName = "MainPanel")
+        {
+            AddButton("OK", "OK", panelName, new Point(Width - 160, Height - 30), 70, 20);
+            Buttons.Last<Button>().onClick += new EHandler(OkButtonClicked);
+        }
+
+        public void AddCancelButton(String panelName = "MainPanel")
+        {
+            AddButton("Cancel", "Cancel", panelName, new Point(Width - 80, Height - 30), 70, 20);
+            Buttons.Last<Button>().onClick += new EHandler(CancelButtonClicked);
+        }
+
+        public float GetVlaue(String fieldName)
+        {
+            float value = 0f;
+            Fields.TryGetValue(fieldName, out value);
+            return value;
+        }
         /// <summary>
         /// LoadContent will be called once per game and is the place to load
         /// all of your content.
@@ -133,14 +249,13 @@ namespace Test
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
-            ButtonTexture = Game.Content.Load<Texture2D>("GUI\texButton");
-            TextBoxTexture = Game.Content.Load<Texture2D>("GUI\texTextBox");
-            PenalTexture = Game.Content.Load<Texture2D>("GUI\texForm");
-            Font = Game.Content.Load<SpriteFont>("GUI\arial");
-            frm = new Form("MyForm", "", 
-                new Rectangle(Position.X, Position.Y, Width, Height), 
-                PenalTexture, Font, Color.Black);
-            
+            ButtonTexture = Game.Content.Load<Texture2D>("texButton");
+            TextBoxTexture = Game.Content.Load<Texture2D>("texTextBox");
+            PanelTexture = Game.Content.Load<Texture2D>("texForm");
+            Font = Game.Content.Load<SpriteFont>("Arial");
+            Frm = new Form("MyForm", "",
+                new Rectangle(Position.X, Position.Y, Width, Height),
+                PanelTexture, Font, Color.Black);
         }
 
         /// <summary>
@@ -152,9 +267,16 @@ namespace Test
             // TODO: Add your update code here
             KeyboardState keyboardState = Keyboard.GetState();
             MouseState mouseState = Mouse.GetState();
-
-            frm.Update(mouseState, keyboardState);
-
+            if (Show)
+            {
+                //Frm.Update(mouseState, keyboardState);
+                foreach (Form frm in Panels.Values)
+                {
+                    //if (frm != null)
+                    frm.Update(mouseState, keyboardState);
+                }
+                Frm.Update(mouseState, keyboardState);
+            }
             base.Update(gameTime);
         }
 
@@ -166,11 +288,17 @@ namespace Test
         {
 
             // TODO: Add your drawing code here
-
-            spriteBatch.Begin();
-            frm.Draw(spriteBatch);
-            spriteBatch.End();
-
+            if (Show)
+            {
+                spriteBatch.Begin();
+                //Frm.Draw(spriteBatch);
+                Frm.Draw(spriteBatch);
+                foreach (Form frm in Panels.Values)
+                {
+                    frm.Draw(spriteBatch);
+                }
+                spriteBatch.End();
+            }
 
             base.Draw(gameTime);
         }
