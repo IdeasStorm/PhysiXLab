5bc7ceb92afc11c802e4cd98defd2ad550058d1a
Mohammed Aljobory <mhdaljobory@gmail.com>
2012-02-03 04:40:06 +0300

    Added Drawable interface

---
+++ PhysicsLab/PhysicsLab/Drawable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PhysicsLab
+{
+    public interface Drawable
+    {
+        void Draw(Camera camera);
+    }
+}
