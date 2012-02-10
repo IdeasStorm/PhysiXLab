f784601f83aa95113df0a4daf70fa2aaddae3a66
adnan khabbaz <adnan.khabbaz@gmail.com>
2012-02-10 23:22:03 +0200

    Fixed joint Bug

--- PhysiXEngine/Joint.cs
+++ PhysiXEngine/Joint.cs
@@ -6,40 +6,53 @@ using Microsoft.Xna.Framework;
 
 namespace PhysiXEngine
 {
-    class Joint
+    public class Joint
     {
         /// <summary>
         /// Holds the two body that connecting with each other
         /// </summary>
         Collidable[] body = new Collidable[2];
 
-        public float constant{get;set;}
+        /// <summary>
+        /// Holds the relative location of the connection for each body, given in local coordinates
+        /// </summary>
+        Vector3[] position = new Vector3[2];
 
-        public Joint(Collidable one, Collidable two, float constant)
+        public float constant { get; set; }
+
+        //float limit;
+
+        public Joint(Collidable one, Collidable two, Vector3 pos1, Vector3 pos2, float constant)//,float limit)
         {
             body[0] = one;
             body[1] = two;
+
+            position[0] = pos1;
+            position[1] = pos2;
+
             this.constant = constant;
+
+            //this.limit = limit;
         }
 
-        public bool addContact(ref Contact contact, int limit)
+        public bool addContact(Contact contact)
         {
             // Calculate the position of each connection point in world coordinates
-            Vector3 onePosWorld = body[0].GetPointInWorldSpace(body[0].Position);
-            Vector3 twoPosWorld = body[1].GetPointInWorldSpace(body[1].Position);
-            
+            Vector3 onePosWorld = body[0].GetPointInWorldSpace(position[0]);
+            Vector3 twoPosWorld = body[1].GetPointInWorldSpace(position[1]);
+
             // Calculate the length of the joint
             Vector3 oneToTwo = twoPosWorld - onePosWorld;
             Vector3 normal = oneToTwo;
-            normal=Vector3.Normalize(normal);
-            
+            normal = Vector3.Normalize(normal);
+
             float length = oneToTwo.Length();
 
             // Check if it is violated
             if (length > constant)
             {
                 contact.body[0] = body[0];
-                
+
                 contact.body[1] = body[1];
                 contact.ContactNormal = normal;
                 contact.ContactPoint = (onePosWorld + twoPosWorld) * 0.5f;
